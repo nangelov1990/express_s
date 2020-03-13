b9b6b2eb48a8c6e2bcfebf37cd4de457b8ee8107
nangelov1990 <nangelov1990@gmail.com>
2020-03-13 13:06:09 +0200

    mongodb native

--- mongo_mongoose/index.js
+++ mongo_mongoose/index.js
@@ -1,9 +1,30 @@
-const mongodb = require('mongodb').MongoClient
-const connection = 'mongodb://localhost:27017/mongo_test'
+const MongoClient = require('mongodb').MongoClient
 
-mongodb
-  .connect(connection)
-  .then(db => {
+// Connection URL
+const url = 'mongodb://localhost:27017/'
+
+// Database name
+const dbName = 'mongo_test'
+
+const client = new MongoClient(url)
+
+client
+  .connect(err => {
+    if (err) console.error(err)
     console.log('MongoDB up and running!')
+    const db = client.db(dbName)
+
+    const cats = db.collection('cats')
+    cats
+      // .insertMany([
+      //   { name: 'Hiko' },
+      //   { name: 'Miko' }
+      // ])
+      .find({ name: 'Hiko' })
+      .toArray()
+      .then(result => {
+        console.log(result)
+      })
+
+    client.close()
   })
-  .catch(console.log)
